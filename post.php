92b0bdb75284c8534367383ab57d8a120e3c40c3
StoyanovConstantin <stoyanov.cons@gmail.com>
2017-11-28 11:24:19 +0600

    update message

--- post.php
+++ post.php
@@ -1,96 +1,25 @@
-<? 
-// ----------------------------конфигурация-------------------------- // 
- 
-$adminemail="stoyanov.cons@gmail.com";  // e-mail админа 
- 
- 
-$date=date("d.m.y"); // число.месяц.год 
- 
-$time=date("H:i"); // часы:минуты:секунды 
- 
-$backurl="http://site.ru/index.html";  // На какую страничку переходит после отправки письма 
- 
-//---------------------------------------------------------------------- // 
- 
-  
- 
-// Принимаем данные с формы 
- 
-$name=$_POST['name']; 
- 
-$email=$_POST['email']; 
- 
-$msg=$_POST['message']; 
- 
-  
- 
-// Проверяем валидность e-mail 
- 
-if (!preg_match("|^([a-z0-9_\.\-]{1,20})@([a-z0-9\.\-]{1,20})\.([a-z]{2,4})|is", 
-strtolower($email))) 
- 
- { 
- 
-  echo 
-"<center>Вернитесь <a 
-href='javascript:history.back(1)'><B>назад</B></a>. Вы 
-указали неверные данные!"; 
- 
-  } 
- 
- else 
- 
- { 
- 
- 
-$msg=" 
- 
- 
-<p>Имя: $name</p> 
- 
- 
-<p>E-mail: $email</p> 
- 
- 
-<p>Сообщение: $msg</p> 
- 
- 
-"; 
- 
-  
- 
- // Отправляем письмо админу  
- 
-mail("$adminemail", "$date $time Сообщение 
-от $name", "$msg"); 
- 
-  
- 
-// Сохраняем в базу данных 
- 
-$f = fopen("message.txt", "a+"); 
- 
-fwrite($f," \n $date $time Сообщение от $name"); 
- 
-fwrite($f,"\n $msg "); 
- 
-fwrite($f,"\n ---------------"); 
- 
-fclose($f); 
- 
-  
- 
-// Выводим сообщение пользователю 
- 
-print "<script language='Javascript'><!-- 
-function reload() {location = \"$backurl\"}; setTimeout('reload()', 6000); 
-//--></script> 
- 
-$msg 
- 
-<p>Сообщение отправлено! Подождите, сейчас вы будете перенаправлены на главную страницу...</p>";  
-exit; 
- 
- } 
- 
+<?php
+// несколько получателей
+$to  = 'stoyanov.c@ya.ru' . ', ';  // обратите внимание на запятую
+$to .= 'stoyanov.constantin@yahoo.com';
+
+// тема письма
+$subject = 'Письмо с моего сайта';
+
+// текст письма
+$message = 'Пользователь' . $_POST['name'] . ' отправил вам письмо:<br />' . $_POST['message'] . '<br />
+Связяться с ним можно по email <a href="mailto:' . $_POST['email'] . '">' . $_POST['email'] . '</a>'
+;
+
+// Для отправки HTML-письма должен быть установлен заголовок Content-type
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+
+// Дополнительные заголовки
+$headers .= 'To: Иван <Ivan@example.com>' . "\r\n"; // Свое имя и email
+$headers .= 'From: '  . $_POST['name'] . '<' . $_POST['email'] . '>' . "\r\n";
+
+
+// Отправляем
+mail($to, $subject, $message, $headers);
 ?>
