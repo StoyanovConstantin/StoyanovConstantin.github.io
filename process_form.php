f5dbb45f03db790b4b64ff140684c247ff30adc8
ITClubWeb <34027763+StoyanovConstantin@users.noreply.github.com>
2025-07-01 14:42:23 +0300

    add send mail

---
+++ process_form.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $option = htmlspecialchars($_POST['option']);
+  $message = htmlspecialchars($_POST['message']);
+
+
+  $to = "stoyanov.c@ya.ru"; // Замените на ваш email
+  $subject = "Сообщение с сайта от " . $name;
+  $body = "Имя: " . $name . "\n" .
+          "Email: " . $email . "\n" .
+          "Услуга: " . $option . "\n" .
+          "Сообщение:\n" . $message;
+  $headers = "From: " . $email;
+
+  if (mail($to, $subject, $body, $headers)) {
+    echo "Сообщение успешно отправлено!";
+  } else {
+    echo "Ошибка при отправке сообщения.";
+  }
+}
+?>
